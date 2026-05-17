a6f321e4752603b5cacc141c726bc35608e9bc18
Dada Vincent <dadaademolav@gmail.com>
2026-05-17 17:59:07 +0100

    switch to vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import { nitro } from "nitro/vite";
-import tailwindcss from "@tailwindcss/vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    tanstackStart(),
-    nitro({ preset: "vercel" }),
-    viteReact(),
-    tailwindcss(),
-    tsConfigPaths(),
-  ],
+  tanstackStart: {
+    server: { entry: "server" },
+  },
+  vite: {
+    plugins: [nitro({ preset: "vercel" })],
+  },
 });
